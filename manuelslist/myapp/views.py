d4971e0336d213ed5d56ca2ddf1e052ef214e3ce
Manuel Aparicio <maar0106@gmail.com>
2020-11-06 22:24:29 -0300

    added new_search templates and frontend features

--- manuelslist/myapp/views.py
+++ manuelslist/myapp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from bs4 import BeautifulSoup
 
 
 def home(request):
     return render(request, 'base.html')
 
+def new_search(request):
+    search = request.POST.get('search')
+    print(search)
+    frontend = {
+        'search': search,
+    }
+    return render(request, 'new_search.html', frontend)
 
